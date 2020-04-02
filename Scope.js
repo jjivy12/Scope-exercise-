38274c08781db0d6f969b46efc3dbb51dce08d10
jjivy12 <62115797+jjivy12@users.noreply.github.com>
2020-04-01 20:43:32 -0400

    Update Scope.js

--- Scope.js
+++ Scope.js
@@ -1,29 +1,48 @@
-var slideShow = {
-	photoList: ['assignment1.png', 'assignment2.png', 'assignment3.png','assignment4.png'],
-	currentPhotoIndex: 0,
-		nextPhoto: function() {
-			this.currentPhotoIndex++;
-		if (this.currentPhotoIndex <= +1) {
-			return this.currentPhotoIndex;
-} else {
-			return ("End of slideshow.");
-			this.currentPhotoIndex--;
-}
-},
-		prevPhoto: function() {
-			this.currentPhotoIndex--;
-	if (this.currentPhotoIndex >= -1) {
-		return this.currentPhotoIndex;
-	} else {
-			return ("Beginning of slideshow.");
-			}
-},
-		getCurrentPhoto: function() {
-		return (this.photoList[this.currentPhotoIndex]);
-}
-};
+var slideshow = {
+  photoList: ['assignment1.png', 'assignment2.png', 'assignment3.png','assignment4.png'],
+  currentPhotoIndex: 0,
+  nextPhoto: function(){
+    if(this.currentPhotoIndex <  (this.photoList.length -1){
+      this.currentPhotoIndex = this.currentPhotoIndex + 1;
+      return this.photoList[this.currentPhotoIndex];
+    } else{
+      return ("End of slideshow");
+    }
+  } ,
+  prevPhoto: function(){
+    if(this.currentPhotoIndex < this.photoList.length && this.currentPhotoIndex !==0){
+      this.currentPhotoIndex = this.currentPhotoIndex - 1;
+      return (this.photoList[this.currentPhotoIndex]);
+    }else {
+      return ("End of slideshow");
+    }
+  } ,
+  getCurrentPhoto: function(){
+    return this.photoList[this.currentPhotoIndex];
+  }
 
+playInterval: null,
 
-console.log(slideShow.nextPhoto());
-console.log(slideShow.getCurrentPhoto());
-console.log(slideShow.prevPhoto());
+    play: function() {
+        var self = this;
+        this.playInterval = setInterval(function(){self.nextPhoto()}, 2000)
+    },
+
+    pause: function() {
+        clearInterval(this.playInterval);
+    }
+
+}
+ console.log(slideshow.photoList);
+ console.log(slideshow.currentPhotoIndex);
+ console.log(slideshow.getCurrentPhoto());
+ console.log(slideshow.nextPhoto());
+ console.log(slideshow.nextPhoto());
+ console.log(slideshow.nextPhoto());
+ console.log(slideshow.nextPhoto());
+ console.log(slideshow.nextPhoto());
+console.log(slideshow.prevPhoto());
+console.log(slideshow.prevPhoto());
+console.log(slideshow.prevPhoto());
+console.log(slideshow.prevPhoto());
+slideshow.play
